17a64816137ba9f0f8f254b64cf78815ee2fd6ae
Ayush Rai <techieayush23@gmail.com>
2026-01-06 22:56:33 +0530

    Initial commit - Spring Boot ecom application

---
+++ src/main/java/com/app/ecom/User.java
@@ -0,0 +1,13 @@
+package com.app.ecom;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+
+}
+
---
+++ src/main/java/com/app/ecom/UserController.java
@@ -0,0 +1,44 @@
+package com.app.ecom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/api/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.fetchAllUsers(),HttpStatus.OK);//this is the first one
+        //There are two ways to do it this is the second one //return ResponseEntity.ok(userService.fetchAllUsers());
+    }
+
+    @GetMapping("/api/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        User user = userService.fetchUser(id);
+        if (user == null) {
+            return new User(); // returns empty user object instead of null
+        }
+        return user;
+    }
+
+
+
+
+    @PostMapping("/api/users")
+    public String createUser(@RequestBody User user) {
+        userService.addUser(user);
+        return "User added Successfully";
+    }
+}
